db0dc15028e3fea00001119ddd561a54d1890fc4
Dae0Kkomi <kdy7760@naver.com>
2025-07-24 22:35:10 +0900

    1463번 최종 정답- 수정본

--- bj1463.py
+++ bj1463.py
@@ -1,28 +1,30 @@
-
+import sys
 a=int(input())
-
+sys.setrecursionlimit(10000000)
 
 
 def cal(a, cnt):
     global temp
+    temp[1]=1
+    temp[2]=1
     if (a==1):
-        if temp>cnt:
-            temp=cnt
+        return
+
+    if(temp[a]!=0): # 이미 계산한적 있을때
+        return temp[a]
             
-    else:
+    if(a!=1):
         if (a%3==0):
-            cnt+=1
-            cal(a//3,cnt)
-            cnt-=1
-        if (a%2==0):        #a==4일때
-            cnt+=1
-            cal(a//2,cnt)
-            cnt-=1
-        cnt+=1           #a==4일때
-        cal(a-1,cnt)
-        
+            cal(a//3,cnt+1)
+            temp[a]+=cnt+1
+        if (a%2==0):              
+            cal(a//2,cnt+1)
+            temp[a]+=cnt+1            
+        cal(a-1,cnt+1)
+        temp[a]+=cnt+1
+    return temp[a]    
     
 
-temp=10000000
+temp=[0 for i in range(0,a+1)]
 cal(a,0)
-print(temp)
+print(min(temp[1:]))
