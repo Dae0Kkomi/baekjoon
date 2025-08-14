b2c950bd2c2a63f425b48a32debead2ad3390d54
Dae0Kkomi <kdy7760@naver.com>
2025-08-15 03:25:52 +0900

    [level 2] Title: 땅따먹기, Time: 222.53 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,8 @@
+def solution(land):
+    answer = 0
+    temp_list=[5,6,7,8]
+    for i in range(1, len(land)):
+        for j in range(4):
+            land[i][j]+=max(land[i-1][0:j]+land[i-1][j+1:])
+    return (max(land[len(land)-1]))
+            
