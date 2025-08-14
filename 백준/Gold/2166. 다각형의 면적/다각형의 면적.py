b068822b7a9e9bfdc3f17d8ec021485bc8b68de8
Dae0Kkomi <kdy7760@naver.com>
2025-08-15 04:58:36 +0900

    [Gold V] Title: 다각형의 면적, Time: 300 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.py
@@ -0,0 +1,13 @@
+a=int(input())
+arr=[]
+for i in range(a):
+    arr.append(list(map(int, input().split())))
+arr.append(arr[0])
+
+temp=0
+temps=0
+for j in range(a):
+    temp+=arr[j][0]*arr[j+1][1]
+    temps+=arr[j+1][0]*arr[j][1]
+
+print(abs(0.5*(temp-temps)))
