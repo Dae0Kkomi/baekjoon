1867b9c772dff116bb4f29ef8b89b2da9253cc7e
Dae0Kkomi <kdy7760@naver.com>
2025-08-24 02:27:50 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,7 @@
 a=int(input())
 arr=list(map(int,input().split()))
-result=[]
-for number in arr:
-    result.append(number/max(arr)*100)
 
-print(sum(result)/a)
+print((sum(arr)/max(arr)*100/a))
+
+
     
