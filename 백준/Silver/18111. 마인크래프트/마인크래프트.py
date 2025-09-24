8756c962a6a5ea1f86ce3c69a300078bb11303f8
Dae0Kkomi <kdy7760@naver.com>
2025-09-24 15:46:43 +0900

    [Silver II] Title: 마인크래프트, Time: 440 ms, Memory: 122000 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -4,40 +4,37 @@ input = sys.stdin.readline
 n,m,b=map(int,input().split())
 
 block = []
-for i in range(n):
+for _ in range(n):      # block들을 1차원 배열로 변경
     block.extend(map(int, input().split()))
+    
 block.sort(reverse=True)
 smin = min(block)
 smax = max(block)
 times=[]
 temps=[]
 temptime=999999990
-for a in range(smin,smax+1):
+for target_block in range(smin,smax+1):
     temp=b
     time=0
-    for iii in block:
-        i=iii-a
-        if i>0:
-            time+=i*2
-            temp+=i
+    for current_block in block:
+        block_cnt=current_block-target_block             #
+        if block_cnt>0:
+            time+=block_cnt*2
+            temp+=block_cnt
             
-        elif i<0:
-            # if (abs(i)>temp):
-            #     time-=1                #인벤토리에 공간이없어서 설치를 못함
-            #     pass
-            if(abs(i)<=temp):
-                temp-=abs(i)
-                time+=abs(i)
+        elif block_cnt<0:
+            if(abs(block_cnt)<=temp):
+                temp-=abs(block_cnt)
+                time+=abs(block_cnt)
             else:
-                time=-1
+                time=-1       # time 을 -1로 설정 (이건 안되는 경우)
                 pass
-    if(temp>=0 and time!=-1): #인벤토리가 부족하지않을때
+    if(temp>=0 and time!=-1): # 인벤토리가 부족하지않을때
         if (temptime>=time):
             temptime=time
-            returns=a
+            returns=target_block
     elif(time==-1):
         break
         
             
 print(temptime, returns)
-
