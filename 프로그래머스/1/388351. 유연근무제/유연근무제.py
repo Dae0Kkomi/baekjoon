f2bcdaa033526c7c7442058c86a5443acc9d4bbe
Dae0Kkomi <kdy7760@naver.com>
2025-10-16 03:41:00 +0900

    [level 1] Title: 유연근무제, Time: 2.28 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,30 +1,16 @@
 def solution(schedules, timelogs, startday):
     result=0
-    for b in range(len(schedules)):
-        temptime=schedules[b]
-        schedules[b]=temptime%100 + ((temptime - temptime%100)//100)*60
-
-        
     for i in range(len(timelogs)):
         isTrue = True
+            
         timelogs[i].pop(7-startday)
         timelogs[i].pop(6-startday)
-        
-        for a in range(5):
-            temptime=timelogs[i][a]
-            timelogs[i][a]=temptime%100 + ((temptime - temptime%100)//100)*60
-
-        
-        for time in timelogs[i]:
+        print(timelogs)
+        for time in timelogs[i]:            
             if time - schedules[i] > 10:
                 isTrue = False
                 break
         if isTrue == True:
-            result+=1
-            continue
-                
-            
-                
-        
+            result+=1                    
     return (result)
     
