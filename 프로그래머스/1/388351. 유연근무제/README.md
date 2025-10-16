bb0a9ac370aad6899974969ab63455ed7a503760
Dae0Kkomi <kdy7760@naver.com>
2025-10-16 16:07:54 +0900

    [level 1] Title: 유연근무제, Time: 3.83 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
 
 ### 성능 요약
 
-메모리: 9.47 MB, 시간: 2.28 ms
+메모리: 9.54 MB, 시간: 3.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 16일 03:40:58
+2025년 10월 16일 16:07:51
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,16 +1,28 @@
 def solution(schedules, timelogs, startday):
     result=0
+    for b in range(len(schedules)):
+        temptime=schedules[b]
+        schedules[b]=temptime%100 + ((temptime - temptime%100)//100)*60
+
+        
     for i in range(len(timelogs)):
         isTrue = True
-            
         timelogs[i].pop(7-startday)
         timelogs[i].pop(6-startday)
-        print(timelogs)
-        for time in timelogs[i]:            
+        
+        for a in range(5):
+            temptime=timelogs[i][a]
+            timelogs[i][a]=temptime%100 + ((temptime - temptime%100)//100)*60
+
+        
+        for time in timelogs[i]:
             if time - schedules[i] > 10:
                 isTrue = False
                 break
         if isTrue == True:
-            result+=1                    
+            result+=1
+            continue
+          
+        
     return (result)
     
